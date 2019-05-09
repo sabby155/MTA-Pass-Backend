063c79a4166e7a4f9719c62773134d32464c24d1
sabby155 <nebula155@gmail.com>
2019-05-09 15:44:22 -0400

    backend files

---
+++ db/migrate/20190507155805_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.integer :lives, default: 0
+      t.integer :points, default: 0
+
+      t.timestamps
+    end
+  end
+end
